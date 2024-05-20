5f5c82bc901aa376d74982b58f37eee641e3eac9
peppapig13132 <peppapig13132@outlook.com>
2024-05-20 21:29:48 +0200

    Create endpoint: /stripe/count-new-subscriptions-2

--- src/controller/stripe.controller.ts
+++ src/controller/stripe.controller.ts
@@ -1,10 +1,45 @@
 import { Response } from "express";
 import asyncHandler from "express-async-handler";
 import dotenv from "dotenv";
+import Stripe from "stripe";
 import { AuthRequest } from "../types/auto-request";
 
 dotenv.config();
 
-export const index = asyncHandler(async (req: AuthRequest, res: Response) => {
-  res.send(req.user)
+const stripeSecretKey = process.env.STRIPE_SECRETKEY || "";
+const stripe = new Stripe(stripeSecretKey);
+
+export const getNewSubscriptionWithDateRange = asyncHandler(async (req: AuthRequest, res: Response) => {
+  if(stripeSecretKey == "") {
+    res.send("stripe key is required");
+  }
+
+  const dateStart = req.body.date_start;
+  const dateEnd = (req.body.date_start < req.body.date_end) ? req.body.date_end : (req.body.date_start + 1);
+  
+  const subscriptions1 = await stripe.subscriptions.list({
+    created: {
+      gte: dateStart - (dateEnd - dateStart),
+      lt: dateStart,
+    },
+  });
+
+  const count1 = subscriptions1.data.length;
+
+  const subscriptions2 = await stripe.subscriptions.list({
+    created: {
+      gte: dateStart,
+      lt: dateEnd,
+    },
+  });
+
+  const count2 = subscriptions2.data.length;
+
+  res.json({
+    ok: true,
+    data: {
+      count1: count1,
+      count2: count2,
+    }
+  });
 });
--- src/routes/stripe.route.ts
+++ src/routes/stripe.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { index } from "../controller/stripe.controller";
+import {
+  getNewSubscriptionWithDateRange,
+} from "../controller/stripe.controller";
 
 const router = Router();
 
-router.post("/index", index);
+router.post("/count-new-subscriptions-2", getNewSubscriptionWithDateRange);
 
 export default router;
