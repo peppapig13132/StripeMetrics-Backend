4b0d34db03be7f0c96ac51a64a051c9a8a6018ed
peppapig13132 <peppapig13132@outlook.com>
2024-05-30 03:54:57 +0200

    :zap: fetchSubscriptions(startDateTimestap, endDateTimestamp, status)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,11 @@
+import dotenv from 'dotenv';
+import Stripe from 'stripe';
 import moment from 'moment';
+
+dotenv.config();
+
+const stripeSecretKey = process.env.STRIPE_SECRETKEY || '';
+const stripe = new Stripe(stripeSecretKey);
 
 export const getFirstDayOfTheMonth = (timestamp: number): number => {
   return moment.unix(timestamp).startOf('month').unix();
@@ -15,3 +22,41 @@ export const getFirstDayOfLastMonth = (timestamp: number): number => {
 export const getDaysOfTheMonth = (timestamp: number): number => {
   return moment.unix(timestamp).daysInMonth();
 }
+
+export const fetchSubscriptions = async (
+    startDateTimestamp: number,
+    endDateTimestamp: number,
+    status: Stripe.Subscription.Status
+  ): Promise<Stripe.Subscription[] | void> => {
+  try {
+    let allSubscriptions: Stripe.Subscription[] = [];
+    let hasMore = true;
+    let startingAfter: string | undefined = undefined;
+
+    while (hasMore) {
+      const params: Stripe.SubscriptionListParams = {
+        created: {
+          gte: startDateTimestamp,
+          lte: endDateTimestamp,
+        },
+        status: status,
+        limit: 100,
+        starting_after: startingAfter,
+      };
+
+      const response = await stripe.subscriptions.list(params);
+      allSubscriptions = allSubscriptions.concat(response.data);
+
+      hasMore = response.has_more;
+      if (hasMore) {
+        startingAfter = response.data[response.data.length - 1].id;
+      } else {
+        startingAfter = undefined;
+      }
+    }
+    
+    return allSubscriptions;
+  } catch (error) {
+    console.error('Error fetching subscriptions from Stripe:', error);
+  }
+};
